a21d3f0b22266932bfe9ce649ac6b3fdc922fef6
Kristian Payne <kristianpayne1@gmail.com>
2024-04-08 21:36:58 +0100

    Setup scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Box } from "@react-three/drei";
+import {
+    Box,
+    PresentationControls,
+    ContactShadows,
+    Environment,
+} from "@react-three/drei";
 
 function App() {
     return (
-        <Canvas>
-            <Box />
+        <Canvas shadows camera={{ position: [0, 0, 10], fov: 25 }}>
+            <color attach="background" args={["#FFBF77"]} />
+            <ambientLight intensity={0.5} />
+            <spotLight
+                position={[10, 10, 10]}
+                angle={0.15}
+                penumbra={1}
+                shadow-mapSize={2048}
+                castShadow
+            />
+            <PresentationControls
+                global
+                config={{ mass: 2, tension: 250 }}
+                snap={{ mass: 2, tension: 150 }}
+                polar={[-Math.PI / 3, Math.PI / 3]}
+                azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+            >
+                <Box />
+            </PresentationControls>
+            {/* <ContactShadows
+                position={[0, -1.4, 0]}
+                opacity={0.75}
+                scale={10}
+                blur={3}
+                far={4}
+            /> */}
+            <Environment preset="sunset" />
         </Canvas>
     );
 }
