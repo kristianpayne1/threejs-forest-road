0e5c948f2b4dcc3e4292fbe5227f4558adefc905
Kristian Payne <kristianpayne1@gmail.com>
2024-04-08 21:16:25 +0100

    Setup environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Box } from "@react-three/drei";
+
+function App() {
+    return (
+        <Canvas>
+            <Box />
+        </Canvas>
+    );
+}
+
+export default App;
