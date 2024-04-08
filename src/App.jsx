996a9e741da5ae1c937fa6f8509020798e909be1
Kristian Payne <kristianpayne1@gmail.com>
2024-04-08 22:53:48 +0100

    Fix z fighting and minor tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { PresentationControls, useGLTF, useTexture } from "@react-three/drei";
+import {
+    ContactShadows,
+    PresentationControls,
+    useGLTF,
+    useTexture,
+} from "@react-three/drei";
 
 function App() {
     return (
-        <Canvas flat shadows camera={{ position: [0, 25, 25], fov: 25 }}>
+        <Canvas
+            flat
+            shadows
+            dpr={[1, 2]}
+            camera={{ position: [0, 55, 65], fov: 10 }}
+        >
             <color attach="background" args={["#FFBF77"]} />
             <PresentationControls
                 global
                 config={{ mass: 2, tension: 250 }}
                 snap={{ mass: 2, tension: 150 }}
-                polar={[-Math.PI / 3, Math.PI / 3]}
+                polar={[-Math.PI / 4.5, Math.PI / 4.5]}
                 azimuth={[-Math.PI / 1.4, Math.PI / 2]}
             >
                 <Model />
             </PresentationControls>
-            {/* <ContactShadows
-                position={[0, -1.4, 0]}
-                opacity={0.75}
-                scale={10}
+            <ContactShadows
+                position={[0, -3.25, 0]}
+                scale={20}
                 blur={3}
                 far={4}
-            /> */}
+            />
         </Canvas>
     );
 }
@@ -35,14 +44,13 @@ function Model() {
     bakedTexture.flipY = false;
 
     return (
-        <group>
+        <group rotation={[0, -Math.PI / 4, 0]}>
             <mesh geometry={baked.geometry}>
                 <meshBasicMaterial map={bakedTexture} />
             </mesh>
-            <mesh
-                geometry={Road_lines.geometry}
-                position={Road_lines.position}
-            />
+            <mesh geometry={Road_lines.geometry} position={Road_lines.position}>
+                <meshBasicMaterial color={"#E7DC00"} />
+            </mesh>
         </group>
     );
 }
